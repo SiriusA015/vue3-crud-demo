0ec6ec62af5e041ffd7b18f746e0e5242b5880b1
SiriusA015 <sirius.a015@gmail.com>
2023-01-17 19:56:42 +0900

    feat/add product-create-component

---
+++ src/views/products/ProductCreate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="product-create">
+    <ProductCreateComponent />
+  </div>
+</template>
+
+<script>
+import ProductCreateComponent from "./../../components/products/create/ProductCreateComponent";
+
+export default {
+  components: {
+    ProductCreateComponent,
+  },
+};
+</script>
